f87a78f140a6e50321c49c2751fc5521589de701
ayush0525 <ayushprakash2505@gmail.com>
2021-09-22 15:08:04 -0700

    commit

---
+++ lib/page/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: [
+        Image.asset("assets/images/login_image.png"),
+      ],
+    ));
+  }
+}
