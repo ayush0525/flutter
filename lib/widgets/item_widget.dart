97f538473db50c08b872d878676e878cf8917116
ayush0525 <ayushprakash2505@gmail.com>
2021-09-27 17:03:03 -0700

    day2completed

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -4,21 +4,37 @@ import 'package:flutter_application_1/models/catelogs.dart';
 class Itemwidget extends StatelessWidget {
   final Item item;
   // ignore: unnecessary_null_comparison
-  const Itemwidget({Key? key, required this.item}) : super(key: key);
+  const Itemwidget({Key? key, required this.item})
+      :
+        // assert( item != null ),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(
-        item.image,
-        height: 70.0,
-        width: 70.0,
+    return Card(
+      elevation: 2.0,
+      child: ListTile(
+        onTap: () {
+          print("${item.name}pressed");
+        },
+        leading: Image.network(
+          item.image,
+          height: 100.0,
+          width: 70.0,
+        ),
+        title: Text(
+          item.name,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          item.descripition,
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal),
+        ),
+        trailing: Text(
+          "\₹${item.price}",
+          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+        ),
       ),
-      title: Text(
-        item.name,
-      ),
-      subtitle: Text(item.descripition),
-      trailing: Text("\₹${item.price}"),
     );
   }
 }
